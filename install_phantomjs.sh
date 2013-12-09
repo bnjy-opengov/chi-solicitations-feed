384e6c56d391fdbd5dff04b87428ebdca9b22442
Nick Bennett <nick@ohrho.com>
2013-12-08 18:26:14 -0600

    Change PhantomJS script to work on systems without a ~/Downloads/ folder.

--- install_phantomjs.sh
+++ install_phantomjs.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-wget https://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-x86_64.tar.bz2 -O ~/Downloads/phantomjs-1.9.1-linux-x86_64.tar.bz2
-cd ~/Downloads/
-tar -xjvf ~/Downloads/phantomjs-1.9.1-linux-x86_64.tar.bz2
-sudo cp ~/Downloads/phantomjs-1.9.1-linux-x86_64/bin/phantomjs /usr/bin/
+wget https://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-x86_64.tar.bz2 -O ~/phantomjs-1.9.1-linux-x86_64.tar.bz2
+cd ~
+tar -xjvf ~/phantomjs-1.9.1-linux-x86_64.tar.bz2
+sudo cp ~/phantomjs-1.9.1-linux-x86_64/bin/phantomjs /usr/bin/
+rm -rf ~/phantomjs-1.9.1-linux-x86_64*
