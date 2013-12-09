512a8735d6d142d6c5cb10a54ee1dd7f2d88ad20
Nick Bennett <nick@ohrho.com>
2013-12-08 18:22:41 -0600

    Add contextlib.closing to ensure webdriver instance is properly closed.

--- driver.py
+++ driver.py
@@ -1,4 +1,5 @@
 # Standard Library Imports
+import contextlib
 import json
 # Third Party Module Imports
 from selenium import webdriver
@@ -8,44 +9,44 @@ from local_settings import USE_PHANTOMJS
 
 def get_business_bids():
     if USE_PHANTOMJS:
-        browser = webdriver.PhantomJS()
+        browser_engine = webdriver.PhantomJS
     else:
-        browser = webdriver.Firefox()
+        browser_engine = webdriver.Firefox
 
-    browser.get('https://webapps1.cityofchicago.org/VCSearchWeb/org/cityofchicago/vcsearch/controller/solicitations/begin.do?agencyId=city')
-    bid_option = browser.find_element_by_xpath('//option[@value="Bid"]')
-    bid_option.click()
+    with contextlib.closing(browser_engine()) as browser:
+        browser.get('https://webapps1.cityofchicago.org/VCSearchWeb/org/cityofchicago/vcsearch/controller/solicitations/begin.do?agencyId=city')
+        bid_option = browser.find_element_by_xpath('//option[@value="Bid"]')
+        bid_option.click()
 
-    to_date = browser.find_element_by_id('toDateId')
-    to_date.send_keys('12/03/2013')
+        to_date = browser.find_element_by_id('toDateId')
+        to_date.send_keys('12/03/2013')
 
-    from_date = browser.find_element_by_id('fromDateId')
-    from_date.send_keys('12/03/2012')
+        from_date = browser.find_element_by_id('fromDateId')
+        from_date.send_keys('12/03/2012')
 
-    search_button = browser.find_element_by_xpath('//input[@alt="search button"]')
-    search_button.click()
+        search_button = browser.find_element_by_xpath('//input[@alt="search button"]')
+        search_button.click()
 
-    print_version_button = browser.find_element_by_xpath('//img[@src="/VCSearchWeb/org/cityofchicago/vcsearch/controller/solicitations/../../../../../resources/images/print_version.gif"]')
-    print_version_button.click()
+        print_version_button = browser.find_element_by_xpath('//img[@src="/VCSearchWeb/org/cityofchicago/vcsearch/controller/solicitations/../../../../../resources/images/print_version.gif"]')
+        print_version_button.click()
 
-    # The "Print Version" button opens a new Firefox window
-    # We need to find the handle of the new window and switch to it
-    other_window_handles = [handle for handle in browser.window_handles if handle != browser.current_window_handle]
-    # We're assuming there's only one other window
-    new_window_handle = other_window_handles[0]
+        # The "Print Version" button opens a new Firefox window
+        # We need to find the handle of the new window and switch to it
+        other_window_handles = [handle for handle in browser.window_handles if handle != browser.current_window_handle]
+        # We're assuming there's only one other window
+        new_window_handle = other_window_handles[0]
 
-    browser.switch_to_window(new_window_handle)
+        browser.switch_to_window(new_window_handle)
 
-    results_tbody = browser.find_element_by_xpath('/html/body/table/tbody/tr[2]/td/table/tbody/tr[4]/td/table/tbody')
-    header_row = results_tbody.find_elements_by_tag_name('tr')[0]
-    result_rows = results_tbody.find_elements_by_tag_name('tr')[1:]
+        results_tbody = browser.find_element_by_xpath('/html/body/table/tbody/tr[2]/td/table/tbody/tr[4]/td/table/tbody')
+        header_row = results_tbody.find_elements_by_tag_name('tr')[0]
+        result_rows = results_tbody.find_elements_by_tag_name('tr')[1:]
 
-    data = []
-    header = [cell.text for cell in header_row.find_elements_by_tag_name('th')]
-    for result_row in result_rows:
-        results = [cell.text for cell in result_row.find_elements_by_tag_name('td')]
-        data.append(dict(zip(header, results)))
-
+        data = []
+        header = [cell.text for cell in header_row.find_elements_by_tag_name('th')]
+        for result_row in result_rows:
+            results = [cell.text for cell in result_row.find_elements_by_tag_name('td')]
+            data.append(dict(zip(header, results)))
     return json.dumps(data)
 
 
